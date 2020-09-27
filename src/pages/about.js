b18bee1ceaa581c48afca2b4770cb56a8b41ff76
Dan Rosenthal <dan.rosenthal@shopify.com>
2020-09-27 19:31:59 -0400

    move content rendering into content transformer

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
-import { BLOCKS, MARKS } from '@contentful/rich-text-types'
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
-
 import Layout from '../components/layout'
 import Hero from '../components/hero'
 import Page from '../components/page'
-import Heading from '../components/heading'
-import Media from '../components/media'
-import Text from '../components/text'
 
 import * as PropTypes from 'prop-types'
+
+import contentfulContentTransformer from '../transformers/contentful-content-transformer'
 
 const propTypes = {
   data: PropTypes.object.isRequired,
@@ -27,39 +23,6 @@ class AboutPage extends React.Component {
       content,
     } = this.props.data.contentfulAboutPage
 
-    const options = {
-      renderNode: {
-        [BLOCKS.EMBEDDED_ENTRY]: node => {
-          const { title, heroImage, description } = node.data.target.fields
-          const headingMarkup = (
-            <Heading level="2">
-              {title["en-US"]}
-            </Heading>
-          );
-          const heroImageMarkup = heroImage && (
-            <img src={heroImage["en-US"].fields.file["en-US"].url} alt={title["en-US"]} />
-          );
-          const descriptionMarkup = description && (
-            <Text>
-              <p>{description["en-US"]}</p>
-            </Text>
-          );
-          return (
-            <Media width="full" image={heroImageMarkup}>
-              {headingMarkup}
-              {descriptionMarkup}
-            </Media>
-          )
-        },
-        [BLOCKS.EMBEDDED_ASSET]: node => {
-          const { title, file } = node.data.target.fields
-          return (
-            <img src={file["en-US"].url} alt={title["en-US"]} />
-          )
-        },
-      },
-    }
-
     return (
       <Layout>
         <Hero
@@ -70,7 +33,7 @@ class AboutPage extends React.Component {
           short={true}
         />
         <Page>
-          {documentToReactComponents(JSON.parse(content.content), options)}
+          {contentfulContentTransformer(content)}
         </Page>
       </Layout>
     )
--- src/transformers/contentful-content-transformer.js
+++ src/transformers/contentful-content-transformer.js
@@ -1,54 +1,63 @@
 import React from 'react'
 
+import { BLOCKS, MARKS } from '@contentful/rich-text-types'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+
 import Heading from '../components/heading'
 import Media from '../components/media'
 import Text from '../components/text'
 
-export default function contentfulContentTransformer(contentfulContent) {
-  return (
-    <>
-      {contentfulContent.content.map((element, index) => {
-        let markup = null
-        if (element.content.length) {
-          const { value } = element.content[0]
-          markup = (
-            <Text key={index}>
-              <p>{value}</p>
-            </Text>
-          )
-        } else if (element.data.target != null) {
-          const { file, description, heroImage, title } = element.data.target.fields
+export default function contentfulContentTransformer(content) {
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ENTRY]: node => {
+        const { title, heroImage, description } = node.data.target.fields
+        const headingMarkup = <Heading level="2">{title['en-US']}</Heading>
+        const heroImageMarkup = heroImage && (
+          <img
+            src={heroImage['en-US'].fields.file['en-US'].url}
+            alt={title['en-US']}
+          />
+        )
+        const descriptionMarkup = description && (
+          <Text>
+            <p>{description['en-US']}</p>
+          </Text>
+        )
+        return (
+          <Media width="full" image={heroImageMarkup}>
+            {headingMarkup}
+            {descriptionMarkup}
+          </Media>
+        )
+      },
+      [BLOCKS.EMBEDDED_ASSET]: node => {
+        const { title, file } = node.data.target.fields
+        return <img src={file['en-US'].url} alt={title['en-US']} />
+      },
+      [BLOCKS.PARAGRAPH]: (_node, children) => {
+        return <Text>{children}</Text>
+      },
+      [BLOCKS.HEADING_1]: (_node, children) => {
+        return <Heading level="1">{children}</Heading>
+      },
+      [BLOCKS.HEADING_2]: (_node, children) => {
+        return <Heading level="2">{children}</Heading>
+      },
+      [BLOCKS.HEADING_3]: (_node, children) => {
+        return <Heading level="3">{children}</Heading>
+      },
+      [BLOCKS.HEADING_4]: (_node, children) => {
+        return <Heading level="4">{children}</Heading>
+      },
+      [BLOCKS.HEADING_5]: (_node, children) => {
+        return <Heading level="4">{children}</Heading>
+      },
+      [BLOCKS.HEADING_6]: (_node, children) => {
+        return <Heading level="4">{children}</Heading>
+      },
+    },
+  }
 
-          const heroImageMarkup = heroImage && (
-            <img src={heroImage.en_US.fields.file.en_US.url} alt={title.en_US} />
-          )
-          const descriptionMarkup = description && (
-            <Text>
-              <p>{description.en_US}</p>
-            </Text>
-          )
-          const imageMarkup = file && <img src={file.en_US.url} alt={title.en_US} />
-          const headingMarkup = (
-            <Heading level="2">
-              {title.en_US}
-            </Heading>
-          );
-
-          const dataMarkup = heroImage ? (
-            <Media width="full" image={heroImageMarkup} key={index}>
-              {headingMarkup}
-              {descriptionMarkup}
-            </Media>
-          ) : (
-            <div key={index}>
-              {imageMarkup}
-              {descriptionMarkup}
-            </div>
-          )
-          markup = dataMarkup;
-        }
-        return <>{markup}</>
-      })}
-    </>
-  )
+  return documentToReactComponents(JSON.parse(content.content), options);
 }
