036fd54cb7d169369ab8a27a98c459e489902e05
Dan Rosental <dan.rosenthal@shopify.com>
2021-01-31 15:43:10 -0500

    remove react simple lightbox

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { SRLWrapper } from 'simple-react-lightbox';
 
 import styles from './gallery.module.scss'
 
@@ -7,10 +6,10 @@ export default function Gallery({ images }) {
   function addEntry(size, image) {
     columns.push({
       size,
-      entries: [<img src={image.file.url} loading="lazy" />]
+      entries: [<img src={image.file.url} loading="lazy" />],
     })
-  };
-  const columns = [];
+  }
+  const columns = []
   images.forEach(image => {
     if (columns[columns.length - 1] == null) {
       if (image.fixed.aspectRatio > 1) {
@@ -21,7 +20,7 @@ export default function Gallery({ images }) {
     } else if (columns[columns.length - 1].size === 1) {
       if (image.fixed.aspectRatio > 1) {
         columns[columns.length - 1].entries.push(<img src={image.file.url} />)
-        columns[columns.length - 1].size = 2;
+        columns[columns.length - 1].size = 2
       } else {
         addEntry(2, image)
       }
@@ -32,21 +31,17 @@ export default function Gallery({ images }) {
         addEntry(2, image)
       }
     }
-  });
+  })
 
-  const columnMarkup = columns.map((column) => {
-    return (<div className={styles.Column}>
-      {column.entries.map((image) => image)}
-    </div>)
-  });
+  const columnMarkup = columns.map(column => {
+    return (
+      <div className={styles.Column}>{column.entries.map(image => image)}</div>
+    )
+  })
 
   return (
-    <SRLWrapper>
-      <div className={styles.GalleryContainer}>
-        <div className={styles.Gallery}>
-          {columnMarkup}
-        </div>
-      </div>
-    </SRLWrapper>
-  );
+    <div className={styles.GalleryContainer}>
+      <div className={styles.Gallery}>{columnMarkup}</div>
+    </div>
+  )
 }
